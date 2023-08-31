1598ab4080e9b735859d886a16902b87285ded78
Cristiano Federico <cristiano.f@jumpgroup.it>
2023-08-31 15:30:33 +0200

    init woocommerce form integration

---
+++ src/FormPlugins/WooCommerceCheckoutForm.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Jumpgroup\Avacy\FormPlugins;
+
+use Jumpgroup\Avacy\Interfaces\FormPlugin;
+use Jumpgroup\Avacy\ConsentSolutionLogger;
+use Jumpgroup\Avacy\ConsentForm;
+
+class WooCommerceCheckoutForm implements FormPlugin
+{
+
+    public static function listen() : void
+    {
+        add_action('woocommerce_checkout_order_processed', [__CLASS__, 'sendFormData']);
+    }
+
+    public static function convertToConsentForm($order_id) : ConsentForm
+    {
+        $order = wc_get_order($order_id);
+
+        $identifier = get_option('avacy_identifier'); // TODO: get identifier from settings
+        $ipAddress = $_SERVER['REMOTE_ADDR'];
+        $proofs = json_encode('<html>...</html>');
+
+        // TODO: get legal notices from settings
+        $legalNotices = [
+            ["name" => "privacy_policy"],
+            ["name" => "cookie_policy"]
+        ];
+
+        // TODO: get preferences from settings
+        $preferences = [
+            [
+                "name" => "newsletter",
+                "accepted" => true
+            ],
+            [
+                "name" => "updates",
+                "accepted" => true
+            ]
+        ];
+
+        return new ConsentForm(
+            name: '',
+            mail: '',
+            identifier: $identifier,
+            ipAddress: $ipAddress,
+            proofs: $proofs,
+            legalNotices: $legalNotices,
+            preferences: $preferences
+        );
+    }
+
+    public static function sendFormData($order_id) : void
+    {
+        $form = self::convertToConsentForm($order_id);
+        ConsentSolutionLogger::send($form);
+    }
+}
