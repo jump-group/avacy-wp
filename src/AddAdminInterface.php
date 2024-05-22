2c5af70e6825b84dcb85bb44f98a0aa61219d29f
Fabio <fabio.p@jumpgroup.it>
2024-05-22 11:18:11 +0200

    Adde _wpnonce security check

--- src/AddAdminInterface.php
+++ src/AddAdminInterface.php
@@ -86,6 +86,9 @@ class AddAdminInterface
   }
   
   public static function AvacyAdminSave() {
+    if ( !isset($_REQUEST['_wpnonce']) || !wp_verify_nonce( $_REQUEST['_wpnonce'], 'avacy-plugin-settings-group-options' ) ) {
+      die( 'Security check' ); 
+    } 
     $redirect_to = isset($_POST['redirectToUrl']) ? esc_url($_POST['redirectToUrl']) : '';
     $tenant = isset($_POST['avacy_tenant']) ? sanitize_text_field($_POST['avacy_tenant']) : '';
     $webspaceKey = isset($_POST['avacy_webspace_key']) ? sanitize_text_field($_POST['avacy_webspace_key']) : '';
