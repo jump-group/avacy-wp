81616311f2337c2724f7bc57f4e9c2720931e045
Cristiano Federico <cristiano.f@jumpgroup.it>
2025-05-09 13:25:43 +0200

    do not ignore vendor/composer and autoload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'jumprock_packages/avacy',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'bfb08c2a7698b073be2caf662436853f89691463',
+        'type' => 'wordpress-muplugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'jumprock_packages/avacy' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'bfb08c2a7698b073be2caf662436853f89691463',
+            'type' => 'wordpress-muplugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
