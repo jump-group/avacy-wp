ce4192066d3a48ea56ded02a9bafa161d84c9674
cristiano-federico-jump <cristiano.federico.92@gmail.com>
2025-05-28 18:19:50 +0200

    handle new account token when fetching cookie banner json

--- src/EnqueueBanner.php
+++ src/EnqueueBanner.php
@@ -13,7 +13,14 @@ class EnqueueBanner {
 
     public static function enqueueScripts() {
         $avacy_team = esc_attr(get_option('avacy_tenant'));
-        $avacy_uuid = esc_attr(get_option('avacy_webspace_key'));
+
+        $avacy_account_token = esc_attr(get_option('avacy_webspace_key'));
+        if(strpos($avacy_account_token, '|')) {
+            $avacy_account_token = explode('|', $avacy_account_token);
+            $avacy_uuid = $avacy_account_token[1];
+        } else {
+            $avacy_uuid = esc_attr(get_option('avacy_webspace_key'));
+        }
 
         if (!empty($avacy_team) && !empty($avacy_uuid)) {
             wp_enqueue_script( 'avacy-stub', 'https://jumpgroup.avacy-cdn.com/current/dist/oilstub.min.js', array(), '1.0.0', false );
